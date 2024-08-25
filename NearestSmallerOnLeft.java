806a1d9118adff05ef000ca6f352e62171b3e668
Premraj Sahu <sahupremraj2@gmail.com>
2024-08-25 12:21:49 +0530

    Update NearestSmallerOnLeft.java

--- NearestSmallerOnLeft.java
+++ NearestSmallerOnLeft.java
@@ -5,7 +5,7 @@ public class NearestSmallerOnLeft {
     public static void main(String[] args) {
         int[] arr = {1, 3, 0, 2, 5};
         int[] result = new int[arr.length];
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>(); //Prefer a Deque over Stack
         for (int i=0; i< arr.length; i++) {
             while(!stack.isEmpty() && stack.peek() >= arr[i]) {
                 stack.pop();
