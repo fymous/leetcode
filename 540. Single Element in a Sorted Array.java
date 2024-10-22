3c0e0b54acd1dff36eca5d24667ea79e7120be96
Premraj Sahu <sahupremraj2@gmail.com>
2024-10-22 19:22:24 +0530

    Create 540. Single Element in a Sorted Array.java
    2 pointers.

---
+++ 540. Single Element in a Sorted Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int p = 0;
+        int n1 = nums.length-1;
+        int n = nums.length-1;
+        if(n==0) return nums[0];
+        while(p<n) {
+            if(p+1 < n1 && nums[p] == nums[p+1]) p = p+2;
+            else return nums[p];
+            if(n-1 > 0 && nums[n] == nums[n-1]) n = n-2;
+            else return nums[n];
+        }
+        return nums[p];
+    }
+}
+
+//not a binary search solution. mostly 2 pointer. Still O(logn)
